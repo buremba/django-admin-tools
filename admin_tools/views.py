8e137fb1f5a12901c2c8fcdd62b6e8e5b5d1252d
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-02-07 16:01:25 +0200

    Update views.py

--- admin_tools/views.py
+++ admin_tools/views.py
@@ -43,9 +43,9 @@ def search(request):
     q = request.GET.get('qs')
     if q is not None:
         for model, model_admin in admin.site._registry.items():
+            meta = model._meta
             search_fields = [field for field in model_admin.search_fields if not hasattr(meta.get_field(field), 'foreign_related_fields') or len(meta.get_field(field).foreign_related_fields) == 0]
             if len(search_fields):
-                meta = model._meta
                 results = get_search_results(search_fields, model.objects.all(), q).count()
                 total_sum += results
                 if results:
