05547f452cdcf2c582035902786e16050967d7d7
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-02-07 18:27:55 +0200

    Update views.py

--- admin_tools/views.py
+++ admin_tools/views.py
@@ -45,7 +45,7 @@ def search(request):
     if q is not None:
         for model, model_admin in admin.site._registry.items():
             meta = model._meta
-            fields = []
+            search_fields = []
             for field_name in model_admin.search_fields:
                 try:
                     field = meta.get_field(field_name)
@@ -53,9 +53,8 @@ def search(request):
                         continue
                 except FieldDoesNotExist, e:
                     pass
-                fields.append(field_name)
+                search_fields.append(field_name)
                 
-            search_fields = [field for field in model_admin.search_fields if not hasattr(meta._name_map, field) or (not hasattr(meta.get_field(field), 'foreign_related_fields') or len(meta.get_field(field).foreign_related_fields) == 0)]
             if len(search_fields):
                 results = get_search_results(search_fields, model.objects.all(), q).count()
                 total_sum += results
